b3b6f6851fef1ea5043da6e161cdf9705e18f350
Hanivan <Hanivan@users.noreply.github.com>
2022-08-19 13:08:33 +0700

    update: import cron module

--- libs/commons/src/playwright/playwright.module.ts
+++ libs/commons/src/playwright/playwright.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { RedisModule } from '@libs/commons/redis/redis.module';
 import { PlaywrightService } from './playwright.service';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [RedisModule, ConfigModule.forRoot({ envFilePath: 'env.example' })],
+  imports: [
+    RedisModule,
+    ConfigModule.forRoot({ envFilePath: 'env.example' }),
+    ScheduleModule.forRoot(),
+  ],
   providers: [PlaywrightService],
   exports: [PlaywrightService],
 })
